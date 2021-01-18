80c56269a398bac8660f107f15e8177c6ae57a60
Tal <tal.yaron@gmail.com>
2021-01-18 09:06:10 +0200

    cont

--- src/view/Pages/Course/Course.js
+++ src/view/Pages/Course/Course.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Course = () => {
     return (
         <div>
-            
+           <input type='text' /> 
         </div>
     )
 }
--- src/view/Pages/HandleCourses/HandleCourses.js
+++ src/view/Pages/HandleCourses/HandleCourses.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
+const courses = [{
+    name: 'tennis',
+    instructors: ['Avi'],
+    // date:{
+    //     starting:'date',
+    //     end:'date',
+    //     repeat:[
+    //         {days:1, hoursStart:'12:40', hoursEnd:'15:00'} //the hours should be in date format
+    //     ]
+    // },
+    students: [{
+        name: 'Nil',
+        surname: 'Armstrong',
+        email: 'nil@moon.com',
+        phone: 'string',
+        userId: 'string',
+        courses: [],
+        role: 'student'
+    }
+    ],
+    active: true,
+    center: 'sport'
+}]
+
 export const HandleCourses = () => {
     return (
         <div>
-            
+create UI
         </div>
     )
 }
