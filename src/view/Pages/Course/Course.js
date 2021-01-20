5afe4cbc67fd8fd4d93502c75a4683d43c93b448
Harper Green <harpergreen02@gmail.com>
2021-01-20 10:39:38 +0200

    t1

--- src/view/Pages/Course/Course.js
+++ src/view/Pages/Course/Course.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
+
 export const Course = () => {
+    const course = {
+        name: 'tennis',
+        instructors: ['Avi'],
+        date:'Wednesday, 2:00pm-3:00pm',
+        // date:{
+        //     starting:'date',
+        //     end:'date',
+        //     repeat:[
+        //         {days:1, hoursStart:'12:40', hoursEnd:'15:00'} //the hours should be in date format
+        //     ]
+        // },
+        students: [{
+            name: 'Nil',
+            surname: 'Armstrong',
+            email: 'nil@moon.com',
+            phone: 'string',
+            userId: 'string',
+            courses: [],
+            role: 'student'
+        }
+        ],
+        active: true,
+        center: 'sport',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC_HxGflb-53zVTxUlG_TbBi6aPpPVoarXzQ&usqp=CAU',
+    
+    }
     return (
-        <div>
-           <input type='text' /> 
+        <div className="course">
+           <img src=${course.image}/>
+    <p><button className="chat">chat</button> </p>
         </div>
     )
 }
