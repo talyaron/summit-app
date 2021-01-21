0d8d84f6c749665a5e8cbda72932a0290c2429a3
Jaclyn <jaclynjosephson@gmail.com>
2021-01-21 12:13:26 +0200

    hjbj

--- src/view/Pages/Profile/Profile.js
+++ src/view/Pages/Profile/Profile.js
@@ -8,8 +8,22 @@ const profilesDB = [];
 function Profile() {
 
     return (
+        <div className='profile'>
         <div>
-            <h1> Profile blala </h1>
+            <h1> Profile </h1>
+        </div>
+        {profilesDB.map((profile, index) => {
+            return (
+        <div className='Name' key={index}>
+            Name: {profile.name}
+         </div>
+         <div className='Photo' key={index}>
+             <img className='image' src={profile.image} alt={"photo of" + profile.name }/>
+         </div>
+    </div>
+            )
+        })}
+
         </div>
     )
 }
