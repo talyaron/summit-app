d843be0a4f0ba1a6cbb070a9eca6f61e012aeba5
Jaclyn <jaclynjosephson@gmail.com>
2021-01-21 12:21:25 +0200

    update

--- src/view/Pages/Profile/Profile.js
+++ src/view/Pages/Profile/Profile.js
@@ -9,18 +9,18 @@ function Profile() {
 
     return (
         <div className='profile'>
-        <div>
             <h1> Profile </h1>
-        </div>
         {profilesDB.map((profile, index) => {
             return (
+
         <div className='Name' key={index}>
             Name: {profile.name}
          </div>
+
          <div className='Photo' key={index}>
              <img className='image' src={profile.image} alt={"photo of" + profile.name }/>
          </div>
-    </div>
+         
             )
         })}
 
