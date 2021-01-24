c04c9952b040635e87ad8a43eb5fbb25bcedeb83
Jacob Karlovsky <jacobka@sas.upenn.edu>
2021-01-24 15:31:28 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- src/view/Pages/HandleCourses/HandleCourses.js
+++ src/view/Pages/HandleCourses/HandleCourses.js
@@ -9,18 +9,18 @@ const sortImg = "https://icon-library.com/images/icon-sort/icon-sort-18.jpg"
 const HandleCourses = () => {
 
     const [coursesDB, setCourses] = useState([])
-    useEffect(() => {
-        courseList.onSnapshot(querySnapshot => {
-            let coursesTempArray = [];
-            querySnapshot.forEach(course => {
-                console.log(course.data())
-                coursesTempArray.push(course.data());
-                console.log(coursesTempArray)
-            })
-            setCourses(coursesTempArray)
+    useEffect(()=>{
+        courseList.onSnapshot(querySnapshot=>{
+          let coursesTempArray = [];
+          querySnapshot.forEach(course=>{
+            console.log(course.data())
+            coursesTempArray.push(course.data());
+            console.log(coursesTempArray)
+          })
+          setCourses(coursesTempArray)
         })
-
-    }, [])
+          
+          },[])
 
 
     function AddCourse() {
@@ -30,41 +30,48 @@ const HandleCourses = () => {
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log("hello" + e)
+        console.log("hello"+e)
         let courseName = e.target.children[0].value
         let teacherName = e.target.children[2].value
         let date = e.target.children[4].value
         let image = e.target.children[6].value
         let active = e.target.children[8].checked
         console.log(courseName, teacherName, date, image)
-        DB.collection('courses').add({ name: courseName, instructors: teacherName, image: image, dates: { start: date }, active: active })
+        DB.collection('courses').add({name: courseName, instructors: teacherName, image: image, dates: {start: date}, active: active})
 
         let addingForm = document.getElementById("AddCourseDiv")
         addingForm.style.visibility = "hidden"
     }
 
-    function changeSortDirection(e) {
+    function changeSortDirection(e){
         console.log(e.target.className)
-        if (e.target.className == "imageUp") {
-            e.target.className = "imagedown"
+        if (e.target.className=="imageUp"){
+            e.target.className="imagedown"
         }
-        else {
-            e.target.className = "imageUp"
+        else{
+            e.target.className="imageUp"
         }
-
+        
     }
 
-    function handleClose(e) {
-        let addingForm = document.getElementById("AddCourseDiv")
+function handleClose(e){
+    let addingForm = document.getElementById("AddCourseDiv")
         addingForm.style.visibility = "hidden"
-    }
+}
 
     return (
         <div className='div'>
             <h1 style={{ textAlign: 'center' }}>Courses</h1>
+<<<<<<< Updated upstream
 
             <img id="sortingImage" className="imageUp" onClick={changeSortDirection} src={sortImg} alt="" />
             
+=======
+            <div id="sortOrder">
+                <img id="sortingImage" className="imageUp" onClick={changeSortDirection} src={sortImg} alt=""/>
+                <span>jdjd</span>
+            </div>
+>>>>>>> Stashed changes
             {coursesDB.map((course, index) => {
                 return (
                     <div className='courseBox' key={index}>
@@ -96,8 +103,8 @@ const HandleCourses = () => {
                     <br />
                     Picture URL: <input type="text" placeholder="text here" />
                     <br />
-                    Active:<input id="active" type="checkbox" />
-                    <br />
+                    Active:<input id="active" type="checkbox"/>
+                    <br/>
                     <input type="submit" />
                 </form>
                 <button className='close' onClick={handleClose}>Close</button>
