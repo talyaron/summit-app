bdccc94f5f13dd3d54be65c7d6ff3c8267be8c13
Jaclyn <jaclynjosephson@gmail.com>
2021-01-24 12:37:00 +0200

    fix

--- src/view/Pages/MainCourses/MainCourses.js
+++ src/view/Pages/MainCourses/MainCourses.js
@@ -5,13 +5,13 @@ import { DB } from '../../../control/firebase/firebase.js';
 const mainCourseList = DB.collection("mainCourses")
 const mainCoursesDB = [];
 
-function mainCourses() {
+let MainCourses = function (){
     return(
         <div className='mainCourses'>
             <h1>Main Courses</h1>
         </div>
     )
-}
+    }
 
     export default MainCourses;
-    
+
