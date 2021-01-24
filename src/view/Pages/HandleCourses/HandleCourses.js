6291a68bb610b339bad565e79306d73488ba8ddd
eli <eli@prais.me.uk>
2021-01-24 14:51:21 +0200

    update

--- src/view/Pages/HandleCourses/HandleCourses.js
+++ src/view/Pages/HandleCourses/HandleCourses.js
@@ -3,33 +3,34 @@ import './HandleCourses.css';
 import { DB } from '../../../control/firebase/firebase.js';
 
 const courseList = DB.collection("courses")
+const sortImg = "https://icon-library.com/images/icon-sort/icon-sort-18.jpg"
 
 
 const HandleCourses = () => {
+
     const [coursesDB, setCourses] = useState([])
-    useEffect(() => {
-        courseList.onSnapshot(querySnapshot => {
-            let coursesTempArray = [];
-            querySnapshot.forEach(course => {
-                console.log(course.data())
-                coursesTempArray.push(course.data());
-                console.log(coursesTempArray)
-            })
-            setCourses(coursesTempArray)
+    useEffect(()=>{
+        courseList.onSnapshot(querySnapshot=>{
+          let coursesTempArray = [];
+          querySnapshot.forEach(course=>{
+            console.log(course.data())
+            coursesTempArray.push(course.data());
+            console.log(coursesTempArray)
+          })
+          setCourses(coursesTempArray)
         })
+          
+          },[])
 
-    }, [])
-
-    let addingForm = document.getElementById("AddCourseDiv")
 
     function AddCourse() {
+        let addingForm = document.getElementById("AddCourseDiv")
         addingForm.style.visibility = "visible"
     }
-    
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log("hello" + e)
+        console.log("hello"+e)
         let courseName = e.target.children[0].value
         let teacherName = e.target.children[2].value
         let date = e.target.children[4].value
@@ -37,16 +38,29 @@ const HandleCourses = () => {
         let active = e.target.children[8].checked
         console.log(courseName, teacherName, date, image)
         DB.collection('courses').add({name: courseName, instructors: teacherName, image: image, dates: {start: date}, active: active})
+
+        let addingForm = document.getElementById("AddCourseDiv")
         addingForm.style.visibility = "hidden"
     }
 
-    function handleClose(e) {
-        addingForm.style.visibility = 'hidden'
+    function changeSortDirection(e){
+        console.log(e.target.className)
+        if (e.target.className=="imageUp"){
+            e.target.className="imagedown"
+        }
+        else{
+            e.target.className="imageUp"
+        }
+        
     }
 
     return (
         <div className='div'>
             <h1 style={{ textAlign: 'center' }}>Courses</h1>
+            <div id="sortOrder">
+                <img id="sortingImage" className="imageUp" onClick={changeSortDirection} src={sortImg} alt=""/>
+                <span>jdjd</span>
+            </div>
             {coursesDB.map((course, index) => {
                 return (
                     <div className='courseBox' key={index}>
@@ -79,10 +93,10 @@ const HandleCourses = () => {
                     Picture URL: <input type="text" placeholder="text here" />
                     <br />
                     Active:<input id="active" type="checkbox"/>
-                    <br />
+                    <br/>
                     <input type="submit" />
+
                 </form>
-                <button className='close' onClick={handleClose}>Close</button>
             </div>
         </div>
     )
