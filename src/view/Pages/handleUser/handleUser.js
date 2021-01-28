f3a492e3e5944ea6fd443798a3548bd7a3204f86
PJ <pjslatkin9@gmail.com>
2021-01-27 20:44:17 -0800

    Commit handleUsers JS Code

--- src/view/Pages/handleUser/handleUser.js
+++ src/view/Pages/handleUser/handleUser.js
@@ -1,22 +1,75 @@
-import React from 'react';
-import './Courses.css';
-import React, { useEffect, useState } from 'react';
+import './Users.css';
+import React, {useEffect, useState} from 'react';
 import {getUsers} from "../../../control/firebase/firebase";
+import {setUser} from "../../../control/firebase/set";
 
-function Users () {
+function Users() {
 
     let users = getUsers();
 
-    let myCourses = [basketball, swimming]
 
     return (
-        <div>
-{
-        users.map(User => {
-            return (<div> <p>{User.courses}</p>
-                <p>Courses</p> <p>{User.name}</p> <p>{User.role}</p> <img src={User.photoURL}></img> </div>) //So technically, we need a css framework to do a drop down and/or proper spacing. We don't have one, so we aren't doing it yet.
+        users.map(u => {
+            const [role, setRole] = useState(u.role);
+            <div>
+                {/*
+                <div className="sort">
+                    <div className="dropdown2">
+                        <button className="dropbtn2">name</button>
+                        <div className="dropdown-content2">
+                            <a href="#">instructor</a>
+                            <a href="#">title</a>
+                            <a href="#">name</a>
+                            <a href="#">date added</a>
+                        </div>
+                    </div>
+                </div>
+                */}
+                <div className="gridHandleUsers">
+                    <div className="courseNames">
+                        <ul>
+                            {u.courses.map(c => {
+                                <li>{c.name}</li>
+                            })}
+                        </ul>
+                    </div>
+                    <div className="wordCourse">Courses</div>
+                    <div className="name"> {u.name}</div>
+                    <div className="photoURL" style={{backgroundImage: "url(" + u.photoURL + ")"}}></div>
+                    <div className="role">
+                        <div className="dropdown"/>
+                        <button className="dropbtn">{role}</button>
+                        <div className="dropdown-content">
+                            <a href="#instructor" onClick={e => {
+                                e.preventDefault()
+                                setRole("instructor")
+                                u.role = "instrutor";
+                                setUser(u);
+                            }}>instructor</a>
+                            <a href="#student" onClick={e => {
+                                e.preventDefault()
+                                setRole("student")
+                                u.role = "student";
+                                setUser(u);
+
+                            }}>student</a>
+                            <a href="#public" onClick={e => {
+                                e.preventDefault()
+                                setRole("public")
+                                u.role = "public";
+                                setUser(u);
+
+                            }}>public</a>
+                            <a href="#admin" onClick={e => {
+                                e.preventDefault()
+                                setRole("admin")
+                                u.role = "admin";
+                                setUser(u);
+                            }}>admin</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         })
-      }
-        </div>
     )
 }
