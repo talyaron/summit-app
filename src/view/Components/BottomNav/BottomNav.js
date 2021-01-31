46e08065ae588a98c7d3eb6e697750b32c16ea45
Jaclyn <jaclynjosephson@gmail.com>
2021-01-31 20:50:17 +0200

    end of day

--- src/view/Components/BottomNav/BottomNav.js
+++ src/view/Components/BottomNav/BottomNav.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 let BottomNav = function () {
     return (
         <div className='nav'>
-           <div> <Link to='/MainCouLoginrses'><div id = 'button1' className='nav__button'> Chat </div></Link>  </div> 
+           <div> <Link to='/MainCourses'><div id = 'button1' className='nav__button'> Chat </div></Link>  </div> 
            <div> <Link to='/MainCourses'> <div id = 'button2' className='nav__button'> All Courses </div> </Link> </div> 
            <div> <Link to='/Profile'><div id = 'button3' className='nav__button'> My Courses </div> </Link></div> 
             
