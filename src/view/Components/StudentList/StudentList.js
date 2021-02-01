4f069479295129dc0aeb4ef84e5a919688eb21e4
Harper Green <harpergreen02@gmail.com>
2021-02-01 13:01:31 +0200

    student list updates

--- src/view/Components/StudentList/StudentList.js
+++ src/view/Components/StudentList/StudentList.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import{DB} from '../../../control/firebase/firebase'
+import firebase from 'firebase'
+import 'firebase/firestore'
 import {useState, useEffect} from 'react';
 import './StudentList.css'
 
 export const StudentList= props =>{
     const {courseId}=props;
     let [students, setStudents]=useState([]);
-    let [currentClass, setCurrentClass]=useState('');
+    let [currentClasses, setCurrentClasses]=useState([]);
     let currClass;
 
     try{
@@ -20,10 +22,9 @@ export const StudentList= props =>{
             
                 setStudents(studentsTemp);
             })
-           checkCurrentClass();
-            //console.log("class:", currClass)
-            setCurrentClass(currClass);
-            //need to check current class here and set current class
+            currClass=checkCurrentClass();
+            setCurrentClasses(currClass);
+            console.log("currentClasses",currClass);//test
             return()=>{
                 unsubscribe()
             }
@@ -33,50 +34,63 @@ export const StudentList= props =>{
 
             let EndOfCourse = new Date(new Date().getTime()+ 1000*60*60*2);
             let currentTime = new Date(new Date().getTime()- 1000*60*60*8);
-           
-
+            const currentLessons = []
+        
             DB.collection('courses').doc(courseId).collection('lesson').where('time','>',currentTime).where('time','<', EndOfCourse).orderBy('time', 'desc').onSnapshot(lessonsDB=>{
-                const userTables = []
                 lessonsDB.forEach(lessonDB=>{
                     console.log(lessonDB.data());
-                    userTables.push({...lessonDB.data(), id: lessonDB.id})
+                    currentLessons.push({... lessonDB.data(), id: lessonDB.id})
                 })
-                console.log(userTables)
-                //set a state
             })
-            return "current class";
+            return currentLessons;
         }
 
-        function checkAttendance(e,student){
-            console.log(student);
-            console.log(e.target.checked)
-            //using the id of the current class, need to add the student and their info to the lesson list
+        function checkAttendance(e,student,currentClass){
+            let inClass=e.target.checked;
+            let lessonId=currentClass.id;
+            let studentId=student.uid;
+            if(inClass){
+                DB.collection('courses').doc(courseId).collection('lesson').doc(lessonId).update({
+                    participatingStudents:firebase.firestore.FieldValue.arrayUnion(studentId)
+                })
+            }
+            else{
+                DB.collection('courses').doc(courseId).collection('lesson').doc(lessonId).update({
+                    participatingStudents:firebase.firestore.FieldValue.arrayRemove(studentId)
+                })
+            }
+            
 
             //https://firebase.google.com/docs/firestore/manage-data/add-data#update_elements_in_an_array
         }
 
-    return(
-        <div className="studentList">
-            <div className="header">
-                <div className="headerItem">Students:</div>
-                 <div className="headerItem">Attendance: {currentClass}</div>
+        return(
+            <div className="studentList">
+                {currentClasses.map((currentClass,index)=>{
+                    return(<div key={index} className="currClass">
+                        <div className="header">
+                            <div className="headerItem">Students:</div>
+                            <div className="headerItem">Attendance: {new Date(currentClass.time.seconds*1000).toString()}</div>
+                        </div>
+                        <div className="students">
+                        {students.map((student,index)=>{
+                            return (<div key={index} className="studentAndAttendance">
+                                <div id="student" className="studentAndAttendanceItem">
+                                    <div className="studentImage"><img src={student.image} alt={`student ${student.name}`}/></div>
+                                    <div className="studentName">{student.name}</div>
+                                </div>
+                                <div id="attendance" className="studentAndAttendanceItem">
+                                    <p>In Class?<input type="checkbox" className="check" onChange={e=>{checkAttendance(e,student,currentClass)}} /></p>
+                                </div>
+                            </div>)
+                         })}
+                        </div>
+                    </div>)
+                })}
             </div>
-            <div className="students">
-            {students.map((student,index)=>{
-                return (<div key={index} className="studentAndAttendance">
-                    <div id="student" className="studentAndAttendanceItem">
-                        <div className="studentImage"><img src={student.image} alt={`student ${student.name}`}/></div>
-                        <div className="studentName">{student.name}</div>
-                    </div>
-                    <div id="attendance" className="studentAndAttendanceItem">
-            <p>In Class?<input type="checkbox" className="check" onChange={e=>{checkAttendance(e,student)}} /></p>
-            {/* If the check box is checked I want it to call this function {checkAttendance(student)} */}
-                    </div>
-                </div>)
-            })}
-            </div>
-        </div>
-    )
+        )
+
+
     }catch(e){
         console.log(e);
     }
