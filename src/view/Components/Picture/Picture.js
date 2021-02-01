4828b28350d12b88eee1e850eb898a28acb02054
Tal <tal.yaron@gmail.com>
2021-02-01 09:17:40 +0200

    picture working

--- src/view/Components/Picture/Picture.js
+++ src/view/Components/Picture/Picture.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import './Picture.css';
 import { DB, storage } from '../../../control/firebase/firebase'
 
-const Picture = (initProps) => {
+const Picture = (props) => {
 
-    const {id} = initProps;
+    const { id, image, name } = props;
     const [progress, setProgress] = useState(false)
-    const [logo, setLogo] = useState('')
+    const [logo, setLogo] = useState(image)
+    const [upadteImage, setUpdateImage] = useState(false)
 
 
     function addPicture() {
@@ -17,7 +19,7 @@ const Picture = (initProps) => {
     function getImage(event) {
         try {
             // imageToUpload = image.target.files[0];
-            const ref = storage.ref("/groups/" + id);
+            const ref = storage.ref("/courses/" + id);
             const image = event.target.files[0];
             // const name = image.name;
             const metadata = {
@@ -29,6 +31,7 @@ const Picture = (initProps) => {
 
             console.log(uploadImg)
             uploadImg.on('state_changed', snapshot => {
+                console.log((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
                 setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
 
 
@@ -36,52 +39,68 @@ const Picture = (initProps) => {
             }, e => {
                 console.error(e)
                 setProgress(false);
-                m.redraw();
+
             }, () => { //when finshed upload
 
                 uploadImg.snapshot.ref.getDownloadURL().then(function (downloadURL) {
 
                     setLogo(downloadURL);
 
-                    DB.collection('groups').doc(id).update({ logo: downloadURL })
+                    DB.collection('courses').doc(id).update({ image: downloadURL })
                         .then(() => {
                             setLogo(downloadURL);
                             setProgress(false);
-                            
+                            setUpdateImage(false)
                         })
                 });
             })
         } catch (e) {
             console.error(e);
             setProgress(false);
-            
+            setUpdateImage(false)
         }
     }
 
-    return (
-        <div>
-            <div className="addPicturesPanel" onclick={addPicture}>
-                <input
-                    type="file"
-                    className="addPicture"
-                    id="pictuerToAdd"
-                    onclick={() => { }}
-                    onchange={event => {
-                        getImage(event, vnode);
-                    }}
-                ></input>
-                {progress ?
-                    <div className='uploader'>
-                        <div style={`width: ${progress}%`} />
-                    </div>
-                    :
+    if (!upadteImage) {
+        return (
+            <div className='imageHolder'>
 
+                {
                     logo ? (
-                        <img src={logo} className="imgOption" />
-                    ) : ( <span>הוסיפו תמונה</span>)}
+                        <div style={{backgroundImage: `url(${logo})`}} className="imgOption" alt={`course ${name}`} onClick={()=>{setUpdateImage(true)}}/>
+                    ) : (<span onClick={()=>{setUpdateImage(true)}}>הוסיפו תמונה</span>)
+
+                }
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className='imageHolder'>
+                {
+                    !progress ?
+                        <div className="addPicturesPanel" onClick={addPicture} >
+                            <input
+                                type="file"
+                                className="addPicture"
+                                id="pictuerToAdd"
+                                onClick={() => { }}
+                                onChange={event => {
+                                    getImage(event);
+                                }}
+                            ></input>
+                        </div>
+                        :
+                        <div className='uploader'>
+                            <div style={{ width: `${progress}%` }} />
+                        </div>
+                }
+            </div>
+        )
+    }
+
+
+
+
 }
 
 export default Picture
