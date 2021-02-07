96b7136a2559154fec941b30d45a8bebd5f9cd07
eli <eli@prais.me.uk>
2021-02-07 15:46:52 +0200

    update

--- src/view/Pages/Calendar/Calendar.js
+++ src/view/Pages/Calendar/Calendar.js
@@ -1,8 +1,32 @@
-
+import { DB } from '../../../control/firebase/firebase.js';
+import {useState} from "react"
 
 
 function Calendar() {
+    let calendar=["fff","fff"]
+
+    DB.collection("calendar").get().then(docs=>{
+        docs.forEach(doc=>{
+            calendar.push(doc.data().image)
+            console.log(doc.data().image)
+            console.log(calendar)
+        })
+    })
     
+    return(
+        <div>
+            <h1>Calendar</h1>
+            <div>
+            {calendar.map((doc,index)=>{
+                console.log("hello")
+                console.log(doc+"hello")
+                return(<div><img src={doc} key={index}/></div>)
+                })   
+            }
+            </div>
+        </div>
+        
+    )
 }
 
 export default Calendar
